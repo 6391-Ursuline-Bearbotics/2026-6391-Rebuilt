89e1e1339c75a5fb532e9d9e05e66a5368112761
6391-Bearbotics <uarobotics@ursulinestl.org>
2026-02-10 15:38:19 -0600

    updated intake deploy

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -14,15 +14,15 @@ public final class IntakeConstants {
   public static final boolean deployMotorInverted = false;
   public static final double deployVoltage = 1.0;
   public static final double retractVoltage = -1.0;
-  public static final double deployCurrentThreshold = 40.0; // Stator amps indicating hard stop
+  public static final double deployCurrentThreshold = 15.0; // Stator amps indicating hard stop
   public static final double deployInrushIgnoreTime = 0.15; // Seconds to ignore after motor starts
   public static final double deployStatorCurrentLimit = 60.0;
 
   // Roller motor
   public static final double rollerGearRatio = 1.0;
   public static final boolean rollerMotorInverted = false;
-  public static final double rollerIntakeVelocityRPM = 100.0;
-  public static final double rollerEjectVelocityRPM = -100.0;
+  public static final double rollerIntakeVelocityRPM = 200.0;
+  public static final double rollerEjectVelocityRPM = -200.0;
   public static final double rollerStatorCurrentLimit = 80.0;
   public static final Slot0Configs rollerGains =
       new Slot0Configs().withKP(0.1).withKV(0.12).withKS(0.0);
