52eeab73e412290859668f6124ae2ec566c2c01b
Dan Baker <dbaker@ursulinestl.org>
2026-03-06 08:20:29 -0600

    sprint to final pose for path PID end

--- src/main/java/frc/robot/AutoRoutines.java
+++ src/main/java/frc/robot/AutoRoutines.java
@@ -189,6 +189,9 @@ public class AutoRoutines {
                 // Cross back over bump via Choreo trajectory
                 bumpReturn.cmd(),
 
+                // Sprint to final shooting position after bump (corrects positional error from bump crossing)
+                sprintToPose(bumpReturn.getFinalPose().orElse(new Pose2d())).withTimeout(2.0),
+
                 // Fine-tune aim (back at hub) before shooting
                 aimBackAtHub().withTimeout(1.0),
 
