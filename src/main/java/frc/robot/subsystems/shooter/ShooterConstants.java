870c040267e548fc8b329922711dd2c3bc040cc7
Dan Baker <dbaker@ursulinestl.org>
2026-05-01 07:54:39 -0500

    bump rpm for pass

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -93,8 +93,8 @@ public final class ShooterConstants {
   public static InterpolatingDoubleTreeMap createPassDistanceToRPMMap() {
     var map = new InterpolatingDoubleTreeMap();
     // TODO: Populate with measured passing data
-    map.put(3.6, 4000.0);
-    map.put(5.0, 5250.0);
+    map.put(3.6, 4200.0);
+    map.put(5.0, 5350.0);
     map.put(6.0, 5600.0);
     map.put(6.5, 6000.0);
     return map;
