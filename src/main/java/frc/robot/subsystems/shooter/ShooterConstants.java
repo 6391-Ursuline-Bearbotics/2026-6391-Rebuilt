d490d560eb8d6777ce331b9b8fa3c098cd29559b
6391-Bearbotics <uarobotics@ursulinestl.org>
2026-09-05 12:28:49 -0500

    adjust pass speed

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -93,8 +93,8 @@ public final class ShooterConstants {
   public static InterpolatingDoubleTreeMap createPassDistanceToRPMMap() {
     var map = new InterpolatingDoubleTreeMap();
     // TODO: Populate with measured passing data
-    map.put(3.6, 4200.0);
-    map.put(5.0, 5350.0);
+    map.put(3.6, 3500.0);
+    map.put(5.0, 3550.0);
     map.put(6.0, 5600.0);
     map.put(6.5, 6000.0);
     return map;
@@ -104,7 +104,7 @@ public final class ShooterConstants {
   public static InterpolatingDoubleTreeMap createPassDistanceToAngleMap() {
     var map = new InterpolatingDoubleTreeMap();
     // TODO: Populate with measured passing data
-    map.put(5.0, 40.0);
+    map.put(5.0, 38.0);
     map.put(6.0, 40.0);
     map.put(6.5, 40.0);
     return map;
